fb22938bced8493c91c831bdc317988b4ef802b7
Galang <Franchez.galangfabregas@gmail.com>
2022-09-17 12:41:50 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
     <div className="ui secondary pointing menu" style={{"background-color": "#432386"}}>
-        <Link to="/" className="item" style={{"color": "whitesmoke", "font-size": "1.3rem", "font-weight": "600"}}>
+        <Link to="/Workout-app" className="item" style={{"color": "whitesmoke", "font-size": "1.3rem", "font-weight": "600"}}>
                 Woktout App
         </Link>
 
         <div className="right menu font-color">
-            <Link to="/" className="item " style={{"color": "whitesmoke", "font-size": "1.3rem", "font-weight": "400"}}>All Programs</Link> 
+            <Link to="/Workout-app" className="item " style={{"color": "whitesmoke", "font-size": "1.3rem", "font-weight": "400"}}>All Programs</Link> 
             <Link to="/About" className="item" style={{"color": "whitesmoke", "font-size": "1.3rem", "font-weight": "400"}}>About</Link>  
         </div>
     </div>
